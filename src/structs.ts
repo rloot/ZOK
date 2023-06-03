69daee3d94314dfbf6b5c158ce70299888c6661c
scammi <scammi@gmail.com>
2023-06-03 19:45:20 -0300

    Added square struct

--- src/structs.ts
+++ src/structs.ts
@@ -1,13 +1,13 @@
 import { Field, SmartContract, state, State, method, Poseidon, Bool, Struct } from "snarkyjs";
 export class Square extends Struct({
-    num: Field
+  num: Field
 }) {
-    public check() {
-        // exclusive minimum
-        this._assert(this.num.greaterThanOrEqual(0), "num must be greater or equal than 0")
-    }
-    public _assert(expr: unknown, msg?: string) {
-        if (!expr)
-            throw new Error(msg);
-    }
+  public check() {
+    // exclusive minimum
+    this._assert(this.num.greaterThanOrEqual(0), "num must be greater or equal than 0")
+  }
+  public _assert(expr: unknown, msg?: string) {
+    if (!expr)
+      throw new Error(msg);
+  }
 }
