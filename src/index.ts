2aa0b9454a021a0e0f067afe91908927064fc8df
scammi <scammi@gmail.com>
2023-06-28 16:55:11 -0300

    Add logs

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import { zodToJsonSchema } from "zod-to-json-schema";
 
 import { ZodObject } from "zod";
 import { createEntity } from "./codegen";
+import { getRedirectStatus } from 'next/dist/lib/load-custom-routes';
 
 export function generate(filename: string, schema: ZodObject<any>) {
   // use the right name
@@ -36,14 +37,26 @@ if (!fs.existsSync(casesPathWithBaseDirectory)) {
 
 const cases = require(casesPathWithBaseDirectory);
 
+let generatedStructs: string[] = [];
+
 if(requestedCase) {
   const schema = cases[requestedCase];
   generate(requestedCase, schema)
+  generatedStructs.push(requestedCase);
 } else {
   for (const key in cases) {
     if (Object.hasOwnProperty.call(cases, key)) {
       const schema = cases[key];
       generate(key, schema)
+      generatedStructs.push(key);
     }
   }
 }
+
+
+console.log('Generated structs: \n')
+generatedStructs.forEach(struct => {
+  console.log(`${struct}`);
+});
+console.log(`\nAt path ${casesPathWithBaseDirectory}\n`);
+
